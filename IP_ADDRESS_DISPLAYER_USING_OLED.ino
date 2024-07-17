baf64263cfdaec5a6b335b02f2df93b02735a976
jaiaakashg <jaiaakashg28210@gmail.com>
2024-07-17 10:23:07 +0530

    Add files via upload

---
+++ IP_ADDRESS_DISPLAYER_USING_OLED.ino
@@ -0,0 +1,46 @@
+#include <WiFi.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void displayText(String text) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print(text);
+  display.display();
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+  display.display();
+  delay(2000); 
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  String actualIP = WiFi.localIP().toString();
+  Serial.println("IP Address: " + actualIP);
+  displayText("IP Address:\n" + actualIP);
+}
+
+void loop() {
+}
+
